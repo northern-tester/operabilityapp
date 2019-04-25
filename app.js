db8e84ab06bc78ef2429894067f8d1b5b75e0ee9
Ash Winter <ash_winter@hotmail.co.uk>
2019-04-25 13:31:01 +0100

    More changes to sample app

--- app.js
+++ app.js
@@ -1,47 +1,77 @@
-var express = require('express')
-, routes = require('./routes')
-, user = require('./routes/users')
-, http = require('http')
-, path = require('path')
-, favicon = require('serve-favicon')
-, bodyParser = require('body-parser')
-, EmployeeProvider = require('./db/employeeprovider').EmployeeProvider;
+var express = require("express"),
+  routes = require("./routes"),
+  user = require("./routes/users"),
+  http = require("http"),
+  path = require("path"),
+  favicon = require("serve-favicon"),
+  bodyParser = require("body-parser"),
+  EmployeeProvider = require("./db/employeeprovider").EmployeeProvider;
 
 var app = express();
 
-  app.set('port', process.env.PORT || 3000);
-  app.set('views', path.join(__dirname, 'views'));
-  app.set('view engine', 'pug');
-  app.set('view options', {layout: false});
-  app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-  app.use(bodyParser.json());
-  app.use(bodyParser.urlencoded({ extended: false }));
-  app.use(require('stylus').middleware(__dirname + '/public'));
-  app.use(express.static(path.join(__dirname, 'public')));
+app.set("port", process.env.PORT || 3000);
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "pug");
+app.set("view options", { layout: false });
+app.use(favicon(path.join(__dirname, "public", "favicon.ico")));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(require("stylus").middleware(__dirname + "/public"));
+app.use(express.static(path.join(__dirname, "public")));
 
-var employeeProvider = new EmployeeProvider('localhost', 27017);
+var employeeProvider = new EmployeeProvider("localhost", 27017, "employee_db");
 
-app.get('/', function(req, res){
-  employeeProvider.findAll(function(error, emps){
-    res.render('index', {
-      title: 'Employees',
-      employees: emps
+app.get("/", function(req, res) {
+  employeeProvider.findAll(function(error, employees) {
+    res.render("index", {
+      title: "Employees",
+      employees: employees
     });
   });
 });
 
-app.get('/employee/new', function(req, res){
-  res.render('employee_new', {
-    title: 'New Employee'
+app.get("/employee/new", function(req, res) {
+  res.render("employee_new", {
+    title: "New Employee"
   });
 });
 
-app.post('/employee/new', function(req, res){
-  employeeProvider.save({
-    title: req.query['title'],
-    name: req.query['name']
-  }, function(error, docs) {
-    res.redirect('/')
+app.post("/employee/new", function(req, res) {
+  employeeProvider.save(
+    {
+      title: req.params.title,
+      name: req.params.name
+    },
+    function(error, docs) {
+      res.redirect("/");
+    }
+  );
+});
+
+app.get("/employee/edit/:_id", function(req, res) {
+  employeeProvider.findById(req.params._id, function(error, employee) {
+    res.render("employee_edit", {
+      employee: employee
+    });
+  });
+});
+
+app.post("/employee/edit/:_id", function(req, res) {
+  employeeProvider.update(
+    req.params._id,
+    {
+      title: req.params.title,
+      name: req.params.name
+    },
+    function(error, docs) {
+      res.redirect("/");
+    }
+  );
+});
+
+app.post("/employee/delete/:_id", function(req, res) {
+  employeeProvider.delete(req.params._id, function(error, docs) {
+    res.redirect("/");
   });
 });
 
--- db/employeeprovider.js
+++ db/employeeprovider.js
@@ -1,26 +1,36 @@
-var Db = require("mongodb").Db;
-var Connection = require("mongodb").Connection;
+var db = require("mongodb").Db;
 var Server = require("mongodb").Server;
 var BSON = require("mongodb").BSON;
 var ObjectID = require("mongodb").ObjectID;
+var Connection = require("mongodb").Connection;
+const MongoClient = require("mongodb").MongoClient;
+const assert = require("assert");
 
-EmployeeProvider = function(host, port) {
-  this.db = new Db(
-    "node-mongo-employee",
-    new Server(host, port, { safe: false }, { auto_reconnect: true }, {})
-  );
-  this.db.open(function() {});
+const collectionName = "employees";
+
+EmployeeProvider = function(host, port, dbname) {
+  url = `mongodb://${host}:${port}`;
+  MongoClient.connect(url, function(error, client) {
+    assert.equal(null, error);
+    console.log("Connected successfully to server");
+    db = client.db(dbname);
+
+    db.createCollection("employees", function(error, result) {
+      if (error) throw error;
+      console.log("Collection is created!");
+    });
+  });
 };
 
 EmployeeProvider.prototype.getCollection = function(callback) {
-  this.db.collection("employees", function(error, employee_collection) {
+  db.collection(collectionName, function(error, employee_collection) {
     if (error) callback(error);
     else callback(null, employee_collection);
   });
 };
 
 EmployeeProvider.prototype.findAll = function(callback) {
-  this.getCollection(function(error, employee_collection) {
+  db.collection(collectionName, function(error, employee_collection) {
     if (error) callback(error);
     else {
       employee_collection.find().toArray(function(error, results) {
@@ -31,20 +41,70 @@ EmployeeProvider.prototype.findAll = function(callback) {
   });
 };
 
-EmployeeProvider.prototype.save = function(employees, callback) {
-  this.getCollection(function(error, employee_collection) {
+EmployeeProvider.prototype.findById = function(employeeId, callback) {
+  db.collection(collectionName, function(error, employee_collection) {
+    if (error) callback(error);
+    else {
+      employee_collection.findOne(
+        {
+          _id: employeeId
+        },
+        function(error, result) {
+          if (error) callback(error);
+          else callback(null, result);
+        }
+      );
+    }
+  });
+};
+
+EmployeeProvider.prototype.update = function(employeeId, employees, callback) {
+  db.collection(collectionName, function(error, employee_collection) {
+    if (error) callback(error);
+    else {
+      employee_collection.update(
+        {
+          _id: employeeId
+        },
+        employees,
+        function(error, employees) {
+          if (error) callback(error);
+          else callback(null, employees);
+        }
+      );
+    }
+  });
+};
+
+EmployeeProvider.prototype.save = function(employee, callback) {
+  db.collection(collectionName, function(error, employee_collection) {
     if (error) callback(error);
     else {
       if (typeof employees.length == "undefined") employees = [employees];
-
       for (var i = 0; i < employees.length; i++) {
-        employees = employees[i];
+        employee = employees[i];
         employee.created_at = new Date();
       }
-
-      employee_collection.insert(employees, function() {
+      employee_collection.insertOne(employees, function() {
         callback(null, employees);
       });
+    }
+  });
+};
+
+EmployeeProvider.prototype.delete = function(employeeId, callback) {
+  db.collection(collectionName, function(error, employee_collection) {
+    if (error) callback(error);
+    else {
+      employee_collection.deleteOne(
+        {
+          _id: employeeId
+        },
+        function(error, employee) {
+          if (error) callback(error);
+          else callback(null, employee);
+        }
+      );
     }
   });
 };
